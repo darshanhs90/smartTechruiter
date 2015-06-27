448a4c0c2e79e336ceab3a7cb19f6e3a350aee3c
Haridarshan HS <hsdars@gmail.com>
2015-06-27 02:51:07 -0500

    localhost reverted back

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(5000, appEnv.bind, function() {
+app.listen(appEnv.port, appEnv.bind, function() {
 
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
