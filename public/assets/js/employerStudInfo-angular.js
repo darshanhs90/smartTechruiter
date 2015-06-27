7fda9a220f06acc3dc56608951cd20f3dd656281
Haridarshan HS <hsdars@gmail.com>
2015-06-27 18:08:08 -0500

    js and html changes

--- public/assets/js/employerStudInfo-angular.js
+++ public/assets/js/employerStudInfo-angular.js
@@ -4,8 +4,9 @@ app.controller('myCtrl',function($scope,$location,$http) {
 
 //php request to get data from "listOfNames"
 
-$scope.getInsights=function($val){
+$scope.getInfo=function($val){
 	//get text corresponding to the val and pass the val  
+    //call getPerson info with email id
 $http({
     url: 'http://smarttechruiter.mybluemix.net/message', 
     method: "GET"
@@ -18,24 +19,8 @@ $http({
 
 
 };
-$scope.shortlist=function($val){
-	//send shortlist message and email
-	//get phone num and email from array corresponding the the val and pass it with custom text
-$http({
-    url: 'http://smarttechruiter.mybluemix.net/message', 
-    method: "GET"
- }).success(function(data, status, headers, config) {
-    alert(data);
-    console.log(data);
- });
-
-
-
-
-};
-$scope.offer=function($val){
-	//send job offer message,call and email
-	//get phone num and email from array corresponding the the val and pass it with custom text
+$scope.getSnaScore=function($val){
+//sna score based on the text input
 $http({
     url: 'http://smarttechruiter.mybluemix.net/message', 
     method: "GET"
@@ -44,23 +29,6 @@ $http({
     console.log(data);
  });
 };
-
-
-
-
-
-
-$scope.reject=function($val){
-//send job reject email
-$http({
-    url: 'http://smarttechruiter.mybluemix.net/message', 
-    method: "GET"
- }).success(function(data, status, headers, config) {
-    alert(data);
-    console.log(data);
- });
-};
-
 
 
 });
