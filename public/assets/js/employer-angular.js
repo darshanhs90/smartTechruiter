fd7fa1f47fcd198d1fd8de2de25d62e1e19eb4b5
Haridarshan HS <hsdars@gmail.com>
2015-06-27 03:01:23 -0500

    modification

--- public/assets/js/employer-angular.js
+++ public/assets/js/employer-angular.js
@@ -2,7 +2,7 @@ var app=angular.module('myApp',[]);
 app.controller('myCtrl',function($scope,$http) {
 
 $http({
-    url: 'http://localhost:3000/home', 
+    url: 'http://localhost/home', 
     method: "GET"
  }).success(function(data, status, headers, config) {
     alert(data);
